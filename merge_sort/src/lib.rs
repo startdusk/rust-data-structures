fe1a6a8fa891c1bddfd345394d0b45b0e434b981
startdusk <522383345@qq.com>
2022-02-10 23:26:07 +0800

    feat✨:  add merge sort

---
+++ merge_sort/src/lib.rs
@@ -0,0 +1,61 @@
+use std::fmt::Debug;
+use std::vec::Vec;
+
+pub fn merge_sort<T: PartialOrd + Debug + Copy>(mut v: Vec<T>) -> Vec<T> {
+    // sort the left half,
+    // sort the right half,
+    // bring the sorted halfs together O(n)
+
+    if v.len() <= 1 {
+        return v;
+    }
+
+    let mut res = Vec::with_capacity(v.len());
+    let b = v.split_off(v.len() / 2);
+    let a = merge_sort(v);
+    let b = merge_sort(b);
+
+    // bring them together again add whichever is lowest is lowest the front of a or the front of b
+    let mut a_it = a.into_iter();
+    let mut b_it = b.into_iter();
+    let mut a_peek = a_it.next();
+    let mut b_peek = b_it.next();
+    loop {
+        match a_peek {
+            Some(ref a_val) => match b_peek {
+                Some(ref b_val) => {
+                    if b_val < a_val {
+                        res.push(b_peek.take().unwrap());
+                        b_peek = b_it.next();
+                    } else {
+                        res.push(a_peek.take().unwrap());
+                        a_peek = a_it.next();
+                    }
+                }
+                None => {
+                    res.push(a_peek.take().unwrap());
+                    res.extend(a_it);
+                    return res;
+                }
+            },
+            None => {
+                if let Some(b_val) = b_peek {
+                    res.push(b_val);
+                }
+                res.extend(b_it.clone());
+                return res;
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_merge_sort() {
+        let v = vec![4, 6, 1, 8, 11, 13, 3];
+        let v = merge_sort(v);
+        assert_eq!(v, vec![1, 3, 4, 6, 8, 11, 13])
+    }
+}
