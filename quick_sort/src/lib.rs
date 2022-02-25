988f8e506abbbeacd914e7be2cb9d5b7b4cb01f0
startdusk <522383345@qq.com>
2022-02-25 17:21:30 +0800

    feat✨:  added threaded quicksort

--- quick_sort/src/lib.rs
+++ quick_sort/src/lib.rs
@@ -1,5 +1,7 @@
 use std::fmt::Debug;
 
+use crossbeam;
+
 mod b_rand;
 
 // Move first element to the correct place
@@ -33,6 +35,26 @@ pub fn quick_sort<T: PartialOrd + Debug>(v: &mut [T]) {
     quick_sort(&mut b[1..]); // Middle element already sorted
 }
 
+// T: 'static 只能是静态类型(静态类型只是一种类型)
+pub fn threaded_quicksort_safe<T: PartialOrd + Debug + Send>(v: &mut [T]) {
+    if v.len() <= 1 {
+        return;
+    }
+    let p = pivot(v);
+    println!("{:?}", p);
+
+    let (a, b) = v.split_at_mut(p);
+
+    // 讨论：https://users.rust-lang.org/t/does-a-threaded-quick-sort-necessarily-require-unsafe-and-raw-pointers/49988/3
+    crossbeam::scope(|scope| {
+        scope.spawn(|_| {
+            threaded_quicksort_safe(a);
+        });
+        threaded_quicksort_safe(&mut b[1..]);
+    })
+    .unwrap(); // thread is also implicitly joined here
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -55,5 +77,11 @@ mod tests {
         let mut v = vec![1, 2, 6, 7, 9, 12, 13, 14];
         quick_sort(&mut v);
         assert_eq!(v, vec![1, 2, 6, 7, 9, 12, 13, 14]);
+    }
+    #[test]
+    fn test_quick_sort_threaded() {
+        let mut v = vec![4, 6, 1, 8, 11, 13, 3];
+        threaded_quicksort_safe(&mut v);
+        assert_eq!(v, vec![1, 3, 4, 6, 8, 11, 13]);
     }
 }
