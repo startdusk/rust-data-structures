7c02f72423d146073f5b80441af2e9e0160db131
startdusk <522383345@qq.com>
2022-02-28 16:09:50 +0800

    feat✨:  linkedlist add sorted_insert

--- linkedlist/src/main.rs
+++ linkedlist/src/main.rs
@@ -4,14 +4,25 @@ fn main() {
     ll.push_back(12);
     ll.push_front(1);
 
-    println!("ll = {:?}", ll)
+    println!("ll = {:?}", ll);
     // Output: ll = LinkedList(Some((1, LinkedList(Some((3, LinkedList(Some((12, LinkedList(None))))))))))
+
+    let mut ll2 = LinkedList::new();
+    ll2.sorted_insert(10);
+    ll2.sorted_insert(6);
+    ll2.sorted_insert(7);
+    ll2.sorted_insert(24);
+    ll2.sorted_insert(1);
+    ll2.sorted_insert(100);
+    ll2.sorted_insert(24);
+
+    println!("ll2 = {:?}", ll2)
 }
 
 #[derive(Debug)]
 pub struct LinkedList<T>(Option<(T, Box<LinkedList<T>>)>);
 
-impl<T> LinkedList<T> {
+impl<T: PartialOrd> LinkedList<T> {
     pub fn new() -> Self {
         LinkedList(None)
     }
@@ -29,9 +40,24 @@ impl<T> LinkedList<T> {
             None => self.push_front(data),
         }
     }
-}
 
-// challenge: insert sorted.
-// find the place it needs to go, and call push_front
-// Tips: implement std::cmp::PartialOrd for LinkedList
-// impl<T: PartialOrd> LinkedList<T> {}
+    // challenge: insert sorted.
+    // find the place it needs to go, and call push_front
+    // Tips: implement std::cmp::PartialOrd for LinkedList
+    // impl<T: PartialOrd> LinkedList<T> {}
+    // 顺序插入linkedlist
+    pub fn sorted_insert(&mut self, data: T) {
+        match self.0 {
+            Some((ref mut node, ref mut child)) => {
+                if data.gt(node) {
+                    child.sorted_insert(data);
+                } else if data.eq(node) {
+                    // not todo
+                } else {
+                    self.push_front(data);
+                }
+            }
+            None => self.push_front(data),
+        }
+    }
+}
