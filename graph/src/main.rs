a8d4507762264ee6ddd75035941cb9cdd0dce41d
startdusk <522383345@qq.com>
2022-03-13 22:57:24 +0800

    feat✨:  add graph

--- graph/src/main.rs
+++ graph/src/main.rs
@@ -1,11 +1,114 @@
 use std::cell::RefCell;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+use std::fmt;
 use std::hash::Hash;
-use std::rc::{Rc, Weak};
+use std::rc::Rc;
 
 #[derive(Debug)]
+#[warn(dead_code)]
 pub struct GraphErr {
     msg: String,
+}
+
+pub trait Weighted {
+    fn weight(&self) -> i32;
+}
+
+impl Weighted for i32 {
+    fn weight(&self) -> i32 {
+        *self
+    }
+}
+
+impl<ID: Eq> Route<ID> {
+    pub fn start_rc(pos: ID) -> Rc<Self> {
+        Rc::new(Route {
+            pos,
+            path: None,
+            len: 0,
+        })
+    }
+
+    pub fn contains(&self, id: &ID) -> bool {
+        if self.pos == *id {
+            return true;
+        }
+        match self.path {
+            Some(ref p) => p.contains(id),
+            None => false,
+        }
+    }
+}
+
+impl<ID: fmt::Debug> fmt::Display for Route<ID> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if let Some(ref p) = self.path {
+            write!(f, "{}-{}-", p, self.len)?;
+        }
+        write!(f, "{:?}", self.pos)
+    }
+}
+
+impl<T, E: Weighted, ID: Clone + Hash + Eq> Graph<T, E, ID> {
+    pub fn shortest_path(&self, from: ID, to: ID) -> Option<Rc<Route<ID>>> {
+        let mut visited = HashSet::new();
+        let mut routes = Vec::new();
+        routes.push(Route::start_rc(from));
+        loop {
+            let c_route = routes.pop()?;
+            if to == c_route.pos {
+                return Some(c_route);
+            }
+            if visited.contains(&c_route.pos) {
+                // no point in searching from the same place twice
+                continue;
+            }
+            visited.insert(c_route.pos.clone());
+
+            let exits = self.data.get(&c_route.pos)?;
+            for eid in &exits.1 {
+                let edge = self.edges.get(eid)?;
+                let npos = if edge.1 == c_route.pos {
+                    // opposite side of edge to current pos
+                    edge.2.clone()
+                } else {
+                    edge.1.clone()
+                };
+                let nlen = c_route.len + edge.0.weight();
+                let nroute = Rc::new(Route {
+                    pos: npos,
+                    len: nlen,
+                    path: Some(c_route.clone()), // RC increase
+                });
+                if routes.len() == 0 {
+                    routes.push(nroute);
+                    continue;
+                }
+                // insert into the list sorted
+                let mut iafter = routes.len() - 1;
+                loop {
+                    if routes[iafter].len > nlen {
+                        // lowest element last
+                        routes.insert(iafter + 1, nroute.clone());
+                        break;
+                    }
+                    if iafter == 0 {
+                        // reached end
+                        routes.insert(0, nroute.clone());
+                        break;
+                    }
+                    iafter -= 1;
+                }
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Route<ID> {
+    pos: ID,
+    path: Option<Rc<Route<ID>>>,
+    len: i32,
 }
 
 impl GraphErr {
@@ -113,6 +216,9 @@ fn main() -> Result<(), GraphErr> {
     g.add_edge('i', 'J', 'E', 15)?;
     g.add_edge('j', 'B', 'A', 16)?;
     println!("graph = {:?}", g);
+
+    println!("shortest path A-D = {}", g.shortest_path('A', 'D').unwrap());
+    println!("shortest path H-B = {}", g.shortest_path('H', 'B').unwrap());
 
     Ok(())
 }
